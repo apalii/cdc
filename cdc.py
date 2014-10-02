676f9cb832c6696fec8be386c9ca5bb406350aeb
Andrii Palii <dezz.ua@gmail.com>
2014-10-02 17:28:04 +0300

    discon/args/re/dict

--- cdc.py
+++ cdc.py
@@ -3,13 +3,15 @@
 
 import re
 import telnetlib as t
+import time
 
 '''
-Duration calls checker
- 
+Call monitor 
+*python 2.7 or higher is required
+
+01.10.2014 - normal processing of the parameters and disconnecting were added
+30.09.2014 - re and dict processing was enhanced a bit
 22.09.2014 - Telnet session was added
-How to use : python calls.py <sipenv ip> <duration>
-Example : python calls.py 123.123.123.123 600
 '''
  
 def show_sip_envs():
@@ -18,29 +20,49 @@ def show_sip_envs():
             print sipenv.rstrip()
  
 def duration_checker(host, duration=600):
- 
     uptime,call_id = [],[]
     tn = t.Telnet(host, "5064")
     tn.write("show call list" + "\n")
  
     for i in tn.read_until("OK").split("\n"):
         if re.search('^\S+@\S+',i):
-            x = re.findall('uptime=(\d+\.)', i)
-            uptime.append(''.join(x)[:-1])
-            y = re.findall('(^\S+@\S+)\sid', i)
-            call_id.append(''.join(y)[:-1])
- 
-    tn.write("q" + "\n")
+            x = re.findall(r'uptime=(\d+)', i)
+            if int(x[0]) > duration :
+                uptime.append(''.join(x))
+                y = re.findall(r'(^\S+@\S+):', i)
+                call_id.append(''.join(y))
     calls = dict(zip(uptime, call_id))
+
     for i in calls:
-        if int(i) > duration:
+        if args.show == True:
+            print '{call_id} : {duration}'.format(duration=i, call_id=calls[i])
+            time.sleep(0.1)
+        else : 
             print calls[i]
- 
+
+    if args.disconnect:
+        print '\nCalls will be disconnected in 5 seconds ! You can press ctrl+C to abort.'
+        time.sleep(5)
+        for i in calls.values():
+            tn.write("disconnect " + i + "\n")
+
+    tn.write("q" + "\n")
+    print 'Telnet connection closed' 
+
 if __name__ == "__main__":
     import sys
-    if sys.argv.__len__() <= 2 or sys.argv.__len__() > 3 :
+    import argparse
+    if sys.argv.__len__() == 1 :
         show_sip_envs()
-    elif sys.argv.__len__() == 3:  # script with 2 parameters
-        if not sys.argv[2].isalpha():
-            duration_checker(sys.argv[1], int(sys.argv[2]))
-        else : print 'Invalid argument!'
+    else:
+        p = argparse.ArgumentParser(description='Call Monitor v.2')
+        p.add_argument("--ip","-i", type=str, help="Ip address of the sip env")
+        p.add_argument("--duration","-d",type=int, default=600, help="Max. duration of the call")
+        p.add_argument("--show", action='store_true', help="It will show call_id and duration")
+        p.add_argument("--disconnect", action='store_true', help="This option will disconnect the calls. Be careful!")
+        args = p.parse_args()
+        if args.ip == None : 
+            print 'IP address of the sipenv is required !'
+            sys.exit(0)
+        else:
+            duration_checker(args.ip, args.duration)
