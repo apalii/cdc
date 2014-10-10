5b1386b84b19a2f3a87085c2df619174dab0a878
Andrii Palii <dezz.ua@gmail.com>
2014-10-10 16:23:04 +0300

    +logging/PEP8/README

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
 cdc
 ===
 
-Duration calls checker - it is a simple script which allows to check some call with long duration.
-Script takes two parameters :
-1) IP address of the needed sip environment
-2) Max allowed duration of the call
+Duration calls checker - it is a simple script which allows to check some calls with long duration.
+Script takes the following parameters :
+1) --ip/-i - IP address of the needed sip environment
+2) --duration/-d - Max allowed duration of the call
+3) --show - shows call id and duration with some actions.
+4) --disconnect - !!! It just disconnects the calls if duration exceed !!!
+                      /You have additional 5 second to abort it/
+5) --debug - writes some additional information to the log file(/home/porta-one/call_monitor.log)
 
-In case when call duration exceeds the entered value - script will show the call-id of this call.
 It possible to use cdc.py without parameters in order to see list of the ip addresses of the available sip envs
 on the current sip server.
 
 It seems it is rare used stuff, I've made just for practicing.
+
+10.10.2014 - PEP8 fixes
+07.10.2014 - logging
+01.10.2014 - normal processing of the parameters and disconnecting were added
+30.09.2014 - re and dict processing was enhanced a bit
+22.09.2014 - Telnet session was added
+
+Examples :
+
+13:00:49 [ET_SYS MR43.0] porta-one@etsys.intra:~
+> sudo python2.7 cdc.py --ip 192.168.197.114 -d 60 --debug --disconnect
+
+Will disconnect all calls with duration more then 60 seconds with debuging information. 
--- cdc.py
+++ cdc.py
@@ -1,67 +1,94 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+Call monitor
+*python 2.7 or higher is required'''
 import re
 import telnetlib as t
 import time
+import logging
+import os.path
 
-'''
-Call monitor 
-*python 2.7 or higher is required
+logger = logging.getLogger(__name__)
+try:
+    handler = logging.FileHandler('/home/porta-one/call_monitor.log')
+except IOError as e:
+    print "({})".format(e)
+formatter = logging.Formatter(u'%(asctime)s [LINE:%(lineno)d] %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
-01.10.2014 - normal processing of the parameters and disconnecting were added
-30.09.2014 - re and dict processing was enhanced a bit
-22.09.2014 - Telnet session was added
-'''
- 
+
 def show_sip_envs():
-    with open('/usr/local/etc/active_sipenvs.conf') as file:
-        for sipenv in file:
-            print sipenv.rstrip()
- 
+    ''' Shows sipenvs'''
+    sipenvs = '/usr/local/etc/active_sipenvs.conf'
+    if os.path.isfile(sipenvs):
+        with open(sipenvs) as active_sipenvs_conf:
+            for sipenv in active_sipenvs_conf:
+                print sipenv.rstrip()
+    else:
+        logger.debug('Can not find %s', sipenvs)
+
+
 def duration_checker(host, duration=600):
-    uptime,call_id = [],[]
-    tn = t.Telnet(host, "5064")
-    tn.write("show call list" + "\n")
- 
-    for i in tn.read_until("OK").split("\n"):
-        if re.search('^\S+@\S+',i):
-            x = re.findall(r'uptime=(\d+)', i)
-            if int(x[0]) > duration :
-                uptime.append(''.join(x))
-                y = re.findall(r'(^\S+@\S+):', i)
-                call_id.append(''.join(y))
-    calls = dict(zip(uptime, call_id))
+    '''Parse and collect uptime and call ids'''
+    uptime_list, call_id_list = [], []
+    try:
+        telnet = t.Telnet(host, "5064")
+        telnet.write("show call list" + "\n")
+        for i in telnet.read_until("OK").split("\n"):
+            if re.search('^\S+@\S+', i):
+                uptime = re.findall('uptime=(\d+)', i)
+                if int(uptime[0]) > duration:
+                    uptime_list.append(''.join(uptime))
+                    call_id = re.findall('(^\S+@\S+):', i)
+                    call_id_list.append(''.join(call_id))
+        calls = dict(zip(uptime_list, call_id_list))
+        logger.debug('Connection with [%s] is established', host)
+    except IOError as e:
+        print "({})".format(e)
+        logger.debug('Connection with [%s] is not established', host)
 
     for i in calls:
         if args.show is True:
             print '{call_id} : {duration}'.format(duration=i, call_id=calls[i])
             time.sleep(0.1)
-        else : 
+        else:
             print calls[i]
 
     if args.disconnect:
-        print '\nCalls will be disconnected in 5 seconds ! You can press ctrl+C to abort.'
+        print '\nCalls will be disconnected in 5 seconds ! \
+        You can press ctrl+C to abort.'
         time.sleep(5)
         for i in calls.values():
-            tn.write("disconnect " + i + "\n")
+            telnet.write("disconnect " + i + "\n")
+            logger.debug('Call %s was disconnected', i)
 
-    tn.write("q" + "\n")
-    print 'Telnet connection closed' 
+    telnet.write("q" + "\n")
+    logger.debug('Telnet connection to %s is closed', host)
 
 if __name__ == "__main__":
     import sys
     import argparse
-    if len(sys.argv) == 1 :
+    if len(sys.argv) == 1:
         show_sip_envs()
     else:
-        p = argparse.ArgumentParser(description='Call Monitor v.2')
-        p.add_argument("--ip","-i", type=str, help="Ip address of the sip env")
-        p.add_argument("--duration","-d",type=int, default=600, help="Max. duration of the call")
-        p.add_argument("--show", action='store_true', help="It will show call_id and duration")
-        p.add_argument("--disconnect", action='store_true', help="This option will disconnect the calls. Be careful!")
-        args = p.parse_args()
-        if args.ip is None : 
+        parser = argparse.ArgumentParser(description='Call Monitor v.2')
+        parser.add_argument("--ip", "-i", type=str,
+                            help="Ip address of the sip env")
+        parser.add_argument("--duration", "-d", type=int, default=600)
+        parser.add_argument("--show", action='store_true',
+                            help="Shows call_id and duration")
+        parser.add_argument("--disconnect", action='store_true',
+                            help="This option will disconnect the calls.")
+        parser.add_argument("--debug", action='store_true', help="Debug")
+        args = parser.parse_args()
+        if args.debug:
+            logger.setLevel(logging.DEBUG)
+        else:
+            logger.setLevel(logging.INFO)
+        if args.ip is None:
             print 'IP address of the sipenv is required !'
             sys.exit(0)
         else:
